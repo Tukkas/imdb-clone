d8fd42ef0d3ef2863bd13c60e5f5b1d9faaf56f7
Tautvydas <u.tautvydas@outlook.com>
2026-03-16 16:12:19 +0200

    Implemented database schema and migrations

---
+++ database/migrations/2026_03_16_134557_create_movie_suggestions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movie_suggestions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->integer('year');
+            $table->text('description')->nullable();
+            $table->string('source_link')->nullable();
+            $table->string('status')->default('pending');
+            $table->text('admin_note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movie_suggestions');
+    }
+};
